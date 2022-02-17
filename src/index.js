8c74c53e0486df2d926d506b5ba8f21e58681867
zero <35671110+zeroclutch@users.noreply.github.com>
2022-02-16 22:49:44 -0800

    Export all created structures

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 /**
  * Export all structures
  */
-
 exports.Client = require('./structures/Client.js')
+
+exports.Circuit     = require('./structures/Circuit')
+exports.Constructor = require('./structures/Constructor')
+exports.Driver      = require('./structures/Driver')
+exports.GrandPrix   = require('./structures/GrandPrix')
+exports.Player      = require('./structures/Player')
